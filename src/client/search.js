b09947927355fb534b8f16d6b46e0d8750b45bc8
suylong <khou.suylong@gmail.com>
2020-08-25 16:21:08 +0700

    added leaflet, react-leaft and render a map in SearchClient

--- src/client/search.js
+++ src/client/search.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import styles from '../styles.module.css'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const ClientSearch = ({ text }) => {
-  return <h1 className={styles.test}>This is client</h1>
+  const position = [51.505, -0.09]
+  return(
+    <div>
+      <h1 className={styles.test}>This is client</h1>
+      <div style={{width: '600px', height: '200px', position: 'absolute'}}>
+        <Map center={position} zoom={13}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        </Map>
+      </div>
+    </div>
+  )
 }
 
 export default ClientSearch;
