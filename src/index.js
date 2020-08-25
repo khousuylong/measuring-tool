b1ffbf646dff292a6fe0063df166490ede700d6e
suylong <khou.suylong@gmail.com>
2020-08-25 14:08:20 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <h1 className={styles.test}>This is H1: Example Component: {text}<h2>And this is h2</h2></h1>
 }
