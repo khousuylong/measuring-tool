b22e0b03b3d3dfd2c182b71ca7e8324821a86bd7
suylong <khou.suylong@gmail.com>
2020-08-26 13:51:41 +0700

    setup graphql client

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,47 @@
 import React from 'react'
-
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { AdminSearch, ClientSearch } from 'search'
 import 'search/dist/index.css'
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
 
 const App = () => {
-  return <React.Fragment><AdminSearch /><ClientSearch /></React.Fragment>
+  return(
+    <ApolloProvider client={client}>
+      <ExchangeRates />
+      <React.Fragment><AdminSearch /><ClientSearch /></React.Fragment>
+    </ApolloProvider>
+  ) 
 }
 
 export default App
