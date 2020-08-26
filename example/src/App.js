3a9242cff84820f22946841bb634d95cdb09e023
suylong <khou.suylong@gmail.com>
2020-08-26 14:02:43 +0700

    made grahpql query working in admin

--- example/src/App.js
+++ example/src/App.js
@@ -3,42 +3,15 @@ import { ApolloProvider } from '@apollo/client';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { AdminSearch, ClientSearch } from 'search'
 import 'search/dist/index.css'
-import { useQuery, gql } from '@apollo/client';
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
-
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
   cache: new InMemoryCache()
 });
 
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
-}
-
 const App = () => {
   return(
     <ApolloProvider client={client}>
-      <ExchangeRates />
       <React.Fragment><AdminSearch /><ClientSearch /></React.Fragment>
     </ApolloProvider>
   ) 
--- src/admin/search.js
+++ src/admin/search.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import styles from '../styles.module.css'
-import { Button } from '@material-ui/core';
+import { Button } from '@material-ui/core'
+import { useQuery, gql } from '@apollo/client'
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        <h1>{currency}</h1>: {rate}
+      </p>
+    </div>
+  ))
+}
 
 const AdminSearch = ({ text }) => {
   return(
     <div>
       <h1 className={styles.test}>This is admin</h1>
       <Button color="primary">Hello World</Button>
+      <ExchangeRates />
     </div>
   )
 }
