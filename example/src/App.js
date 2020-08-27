6b7c8f7a9a68867e217a07f3b869f9ae19398728
suylong <khou.suylong@gmail.com>
2020-08-27 14:48:42 +0700

    created form and save the setting

--- example/src/App.js
+++ example/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/client';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { AdminSearch, ClientSearch } from 'search'
+import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
 import { makeExecutableSchema } from 'graphql-tools';
 import {typeDefs} from './schema/schema';
 import {resolvers} from './schema/resolvers';
@@ -13,16 +15,31 @@ const schema = makeExecutableSchema({
   resolvers
 });
 
+const styles = theme => ({
+  container: {
+    margin: 'auto',
+    maxWidth: '700px',
+    minWidth: '300px',
+    marginTop: '20px'
+  }
+});
+
 const client = new ApolloClient({
   //uri: 'https://48p1r2roz4.sse.codesandbox.io',
   link: new SchemaLink({ schema }),
   cache: new InMemoryCache()
 });
 
-const App = () => {
+const App = props => {
+  const {classes} = props;
   return(
-    <React.Fragment><AdminSearch client={client} /><ClientSearch /></React.Fragment>
+    <React.Fragment>
+      <div className={classes.container}>
+        <AdminSearch client={client} settingId="b67635cc-cb47-4aaf-b37b-42e470acfef3" />
+        <Divider style={{marginTop: 20}} />
+        <ClientSearch />
+      </div>
+    </React.Fragment>
   ) 
 }
-
-export default App
+export default withStyles(styles)(App);
